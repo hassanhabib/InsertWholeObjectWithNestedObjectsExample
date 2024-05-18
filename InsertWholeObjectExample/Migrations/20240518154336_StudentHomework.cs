40fcef0bac1a09fb1337e5ef4c6eb188139ea25b
Hassan Habib <hassanhabib@live.com>
2024-05-18 08:47:26 -0700

    Add project files.

---
+++ InsertWholeObjectExample/Migrations/20240518154336_StudentHomework.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsertWholeObjectExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentHomework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Homeworks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Homeworks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Homeworks_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Homeworks_StudentId",
+                table: "Homeworks",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Homeworks");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ InsertWholeObjectExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InsertWholeObjectExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var storageBroker = new StorageBroker();
+            storageBroker.Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Name = "Moamen",
+                Homeworks = new List<Homework>
+                {
+                    new Homework
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "C# Homework"
+                    },
+
+                    new Homework
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "Java Homework"
+                    }
+                }
+            });
+
+            storageBroker.SaveChanges();
+        }
+    }
+
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Homework> Homeworks { get; set; }
+    }
+
+    public class Homework
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Student Student { get; set; }
+    }
+
+    internal class StorageBroker : DbContext
+    {
+        public StorageBroker() => this.Database.Migrate();
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=OtripleSDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
